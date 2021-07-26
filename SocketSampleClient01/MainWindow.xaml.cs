24078ae61aa6bffc688e8d5d28053fb7d07c47fd
makoto <ppkotonari@gmail.com>
2021-07-26 14:35:46 +0900

    ソケット通信 成功

--- SocketSampleClient01/MainWindow.xaml.cs
+++ SocketSampleClient01/MainWindow.xaml.cs
@@ -1,23 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SocketSampleClient01
 {
@@ -26,6 +16,7 @@ namespace SocketSampleClient01
     /// </summary>
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        #region プロパティ
         /// <summary>
         /// ホスト名
         /// </summary>
@@ -68,25 +59,34 @@ namespace SocketSampleClient01
         /// <summary>
         /// 受信テキスト
         /// </summary>
-        public string RecieveText
+        public string OutputText
         {
-            get => _recieveText;
+            get => _outputText;
             set
             {
-                _recieveText = value;
+                _outputText = value;
                 OnPropertyChanged();
             }
         }
+        #endregion
 
+        #region イベント
+        /// <summary>
+        /// プロパティ変更通知イベント
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region フィールド
         /// <summary>
         /// TcpClientオブジェクト
         /// </summary>
-        private TcpClient _tcpClient = null;
+        private MyTcpClient _tcpClient = null;
 
         /// <summary>
         /// ホスト名
         /// </summary>
-        private string _targetHostName = string.Empty;
+        private string _targetHostName = "127.0.0.1";
 
         /// <summary>
         /// ポート番号
@@ -101,135 +101,49 @@ namespace SocketSampleClient01
         /// <summary>
         /// 受信テキスト
         /// </summary>
-        private string _recieveText = string.Empty;
+        private string _outputText = string.Empty;
+        #endregion
 
-        /// <summary>
-        /// プロパティ変更通知イベント
-        /// </summary>
-        public event PropertyChangedEventHandler PropertyChanged;
-
+        #region コンストラクタ
         /// <summary>
         /// コンストラクタ
         /// </summary>
         public MainWindow()
         {
             InitializeComponent();
-            TargetHostName = GetLocalIPAddress();
-        }
-
-        /// <summary>
-        /// ローカルIPアドレス取得
-        /// </summary>
-        /// <returns>ローカルIPアドレス</returns>
-        private string GetLocalIPAddress()
-        {
-            var host = Dns.GetHostEntry(Dns.GetHostName());
-            foreach (var ip in host.AddressList)
-            {
-                if (ip.AddressFamily == AddressFamily.InterNetwork)
-                {
-                    return ip.ToString();
-                }
-            }
-            throw new Exception("No network adapters with an IPv4 address in the system!");
-        }
-
-        /// <summary>
-        /// TCP接続
-        /// </summary>
-        /// <param name="hostName">ホスト名</param>
-        /// <param name="portNum">ポート番号</param>
-        /// <returns>接続可否</returns>
-        private bool OpenTcpClient(string hostName, int portNum)
-        {
-            try
-            {
-                _tcpClient = new TcpClient(hostName, portNum);
-                return true;
-            }
-            catch (Exception e)
-            {
-                MessageBox.Show(e.Message);
-                return false;
-            }
-        }
-
-        /// <summary>
-        /// TCP切断
-        /// </summary>
-        private void CloseTcpClient()
-        {
-            _tcpClient?.Close();
-        }
-
-        /// <summary>
-        /// メッセージ送信
-        /// </summary>
-        /// <param name="sendText">送信テキスト</param>
-        /// <returns>受信テキスト</returns>
-        private string SendMessage(string sendText)
-        {
-            string recieveMsg = string.Empty;
-
-            using (NetworkStream ns = _tcpClient.GetStream())
-            {
-                ns.ReadTimeout = 1000;
-                ns.WriteTimeout = 1000;
-
-                SendTcp(ns, sendText);
-                recieveMsg = RecieveTcp(ns);
-
-                ns.Close();
-            }
-
-            return recieveMsg;
-        }
-
-        /// <summary>
-        /// TCP送信
-        /// </summary>
-        /// <param name="ns">NetworkStream</param>
-        /// <param name="sendText">送信テキスト</param>
-        private void SendTcp(NetworkStream ns, string sendText)
-        {
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            byte[] sendBytes = Encoding.GetEncoding("Shift_JIS").GetBytes(sendText + '\n');
-            ns.Write(sendBytes, 0, sendBytes.Length);
+        }
+        #endregion
+
+        #region プライベートメソッド
+        /// <summary>
+        /// プロパティ変更通知
+        /// </summary>
+        /// <param name="propertyName"></param>
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
-        /// <summary>
-        /// TCP受信
-        /// </summary>
-        /// <param name="ns">NetworkStream</param>
-        /// <returns>受信テキスト</returns>
-        private string RecieveTcp(NetworkStream ns)
+        private void Main()
         {
-            string recieveMsg = string.Empty;
+            // クライアントインスタンス作成
+            _tcpClient = new MyTcpClient();
 
-            using (MemoryStream ms = new MemoryStream())
-            {
-                byte[] resBytes = new byte[256];
-                int resSize = 0;
+            // 接続試行
+            if (!_tcpClient.TryConnection(TargetHostName, TargetPortNum, 1000)) { return; }
 
-                do
-                {
-                    resSize = ns.Read(resBytes, 0, resBytes.Length);
+            // テキスト送信
+            byte[] data = Encoding.GetEncoding("Shift_JIS").GetBytes(SendCommandText);
+            _tcpClient.Write(data);
 
-                    if(resSize == 0)
-                    {
-                        MessageBox.Show("サーバーが切断しました。");
-                        break;
-                    }
+            // テキスト受信
+            byte[] buffer = _tcpClient.Read((buf) => true, 15000);
+            string recieveText = Encoding.GetEncoding("Shift_JIS").GetString(buffer);
+            OutputText += recieveText + Environment.NewLine;
 
-                    ms.Write(resBytes, 0, resSize);
-
-                } while (ns.DataAvailable || resBytes[resSize - 1] != '\n');
-
-                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-                recieveMsg = Encoding.GetEncoding("Shift_JIS").GetString(ms.GetBuffer());
-            }
-
-            return recieveMsg;
+            // 接続解除
+            _tcpClient.Close();
         }
 
         /// <summary>
@@ -245,7 +159,7 @@ namespace SocketSampleClient01
         }
 
         /// <summary>
-        /// 確認ボタン押下イベントハンドラ
+        /// 確認ボタン押下
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -256,27 +170,110 @@ namespace SocketSampleClient01
         }
 
         /// <summary>
-        /// 送信ボタン押下イベントハンドラ
+        /// 送信ボタン押下
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void BtnSendCommand_Click(object sender, RoutedEventArgs e)
         {
-            if(OpenTcpClient(TargetHostName, TargetPortNum))
+            Task.Run(() => { Main(); });
+        }
+        #endregion
+
+        #region 内部クラス
+        private class MyTcpClient: TcpClient
+        {
+            #region コンストラクタ
+            /// <summary>
+            /// コンストラクタ
+            /// </summary>
+            public MyTcpClient() : base() { }
+            #endregion
+
+            #region パブリックメソッド
+            /// <summary>
+            /// 接続試行
+            /// </summary>
+            /// <param name="addr">ホスト名</param>
+            /// <param name="port">ポート番号</param>
+            /// <param name="ms">タイムアウト時間(ms)</param>
+            /// <returns>接続成否</returns>
+            public bool TryConnection(string addr, int port, int ms)
             {
-                RecieveText = SendMessage(SendCommandText);
+                // 非同期接続
+                IAsyncResult ar = this.BeginConnect(addr, port, null, null);
+
+                // 接続待機
+                WaitHandle handle = ar.AsyncWaitHandle;
+
+                try
+                {
+                    if (!handle.WaitOne(ms, false))
+                    {
+                        // タイムアウト
+                        this.Close();
+                        return false;
+                    }
+                    else
+                    {
+                        // 接続成功
+                        this.EndConnect(ar);
+                        return true;
+                    }
+                }
+                finally
+                {
+                    handle.Close();
+                }
             }
 
-            CloseTcpClient();
-        }
+            /// <summary>
+            /// データ送信
+            /// </summary>
+            /// <param name="data">送信データ</param>
+            public void Write(byte[] data)
+            {
+                NetworkStream ns = this.GetStream();
+                ns.Write(data, 0, data.Length);
+            }
 
-        /// <summary>
-        /// プロパティ変更通知イベントハンドラ
-        /// </summary>
-        /// <param name="propertyName"></param>
-        private void OnPropertyChanged([CallerMemberName]string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            /// <summary>
+            /// データ受信
+            /// </summary>
+            /// <param name="isComplete">受信完了判断処理</param>
+            /// <param name="ms">タイムアウト時間(ms)</param>
+            /// <returns>受信データ</returns>
+            public byte[] Read(Func<byte[], bool> isComplete, int ms)
+            {
+                try
+                {
+                    byte[] buffer = new byte[256];
+                    NetworkStream ns = this.GetStream();
+                    ns.ReadTimeout = ms;
+
+                    while (true)
+                    {
+                        int size = ns.Read(buffer, 0, buffer.Length);
+
+                        if (size == 0)
+                        {
+                            return Array.Empty<byte>();
+                        }
+
+                        if (isComplete(buffer))
+                        {
+                            break;
+                        }
+                    }
+                    return buffer;
+                }
+                catch
+                {
+                    return Array.Empty<byte>();
+                }
+            }
+            #endregion
         }
+        #endregion
     }
 }
